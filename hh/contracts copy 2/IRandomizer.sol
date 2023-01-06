1aa38e5184c13e4bc4b4a20087746cecff169827
daniel assa <danielassa@daniels-MacBook-Pro.local>
2023-01-06 10:49:33 +0200

    backup moved

--- hh/contracts copy 2/IRandomizer.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.8.9;
-
-abstract contract IRandomizer {
-    function random(uint fromblock) virtual public view returns(uint);
-    function safe_random(uint fromblock) virtual public view returns(uint);
-}
--- hh/contracts copy 2/Reverter.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.9;
-
-contract Reverter {
-
-    function ok() public {
-        revert("Reverter Default");
-    }
-}
